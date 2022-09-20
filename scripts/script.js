732bc0149181a75afea92e1ea3f778aaa1b793c4
Stephan Kok <stephan.kok.345@gmail.com>
2022-09-20 17:26:30 +0200

    Responsive part 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // JavaScript Document
 console.log("hi");
+
+var hamButton = document.querySelector("header img:nth-of-type(1)");
+var navBar = document.querySelector("nav");
+
+hamButton.addEventListener("click", doeIets);
+
+function doeIets() {
+    if (navBar.classList.contains("shown")) {
+        navBar.classList.remove("shown");
+    } else {
+        navBar.classList.add("shown");
+    }
+
+}
